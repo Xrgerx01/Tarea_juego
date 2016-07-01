85963984baec88ecb15e9ab7664edc8e0d484bcc
Xrgerx01 <xrgerx@gmail.com>
2016-07-01 09:55:03 -0500

    Add files via upload

---
+++ Contents.swift
@@ -0,0 +1,68 @@
+/*
+
+TAREA VELOCIMETRO AUTOMÓVIL DIGITAL
+
+Versión 1.0
+
+Alumno: Juan Antonio Ramírez Reyes
+
+Entregado: Miércoles, 01 de junio de 2016
+
+*/
+
+import UIKit
+
+enum Velocidades : Int{
+    case
+    Apagado = 0,
+    VelocidadBaja = 20,
+    VelocidadMedia = 50,
+    VelocidadAlta = 120
+   
+    
+    init( velocidadInicial : Velocidades){
+        self = velocidadInicial
+    }
+}
+
+class Auto{
+    var velocidad : Velocidades
+    
+    init(){
+        self.velocidad = Velocidades(velocidadInicial: .Apagado)
+    }
+    
+    func cambioDeVelocidad( ) -> ( actual : Int, velocidadEnCadena: String){
+        var velocidadTexto = ""
+        let velocidadActual = velocidad.rawValue
+        
+        switch velocidad{
+        case .Apagado:
+            velocidad = .VelocidadBaja
+            velocidadTexto = "Apagado"
+        case .VelocidadBaja:
+            velocidad = .VelocidadMedia
+            velocidadTexto = "Velocidad Baja"
+        case .VelocidadMedia:
+            velocidad = .VelocidadAlta
+            velocidadTexto = "Velocidad Media"
+        case .VelocidadAlta:
+            velocidad = .VelocidadMedia
+            velocidadTexto = "Velocidad Alta"
+        
+        }
+        
+        return (actual : velocidadActual, velocidadEnCadena : velocidadTexto)
+    }
+
+}
+
+var auto = Auto()
+
+for i in 1...20{
+    var Resultado = auto.cambioDeVelocidad()
+    print("<<<< \(Resultado.actual) >>>>")
+    print("\(Resultado.velocidadEnCadena)\n")
+}
+
+
